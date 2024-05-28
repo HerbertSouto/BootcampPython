91df01e18c26a7eeda4bce211151fe112f0ed929
Herbert Souto <herbert-93@hotmail.com>
2024-05-28 20:02:35 -0300

    Atualizando a descrição

--- Exercicio3.py
+++ Exercicio3.py
@@ -1,4 +1,4 @@
-#Exercício 4: Validação de Dados de Entrada
+#Exercício 3: Validação de Dados de Entrada
 #Antes de processar os dados de usuários em um sistema de recomendação, você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha fornecido um email válido. Escreva um programa que valide essas condições e imprima "Dados de usuário válidos" ou o erro específico encontrado.
 
 import re
