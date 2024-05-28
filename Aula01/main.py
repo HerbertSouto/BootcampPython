26cdc6cb6e1f985e0c7f8d98ee92a47536f3268b
Herbert Souto <herbert-93@hotmail.com>
2024-05-28 20:55:52 -0300

    código realiado na primeira aula

---
+++ Aula01/main.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salário: "))
+bonus = float(input("Digite seu bônus: "))
+valor_bonus = 1000 + salario * bonus
+print(f"olá {nome}, o seu bônus foi de {valor_bonus} ")
