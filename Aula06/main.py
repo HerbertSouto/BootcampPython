9748c3801735a7b1b30e6288d81aae1469be3ebb
Herbert Souto <herbert-93@hotmail.com>
2024-06-12 15:24:58 -0300

    testando o pre commit

--- Aula06/main.py
+++ Aula06/main.py
@@ -1,23 +1,10 @@
-# Exemplo de código Python para teste do flake8, black e isort
-
-# Teste para o flake8 - estilo de código
 def soma(a, b):
     return a + b
+
 
 resultado = soma(3, 4)
 print("O resultado é:", resultado)
 
-
-# Teste para o black - formatação de código
-lista = [1,2,3,4]
+lista = [1, 2, 3, 4]
 for item in lista:
     print(item)
-
-
-import datetime
-import json
-
-# Teste para o isort - organização de importações
-import os
-import random
-import sys
